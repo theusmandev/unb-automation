93de67f3a4d6057de1de220abe896510d3802cd4
theusmandev <theusmandeveloper@gmail.com>
2026-01-27 16:25:22 +0500

    updated

--- Bulk Thumbnail maker/thumb_with_watermark_.py
+++ Bulk Thumbnail maker/thumb_with_watermark_.py
@@ -4,7 +4,7 @@ import math
 from PIL import Image, ImageEnhance, ImageStat, ImageDraw, ImageFilter, ImageFont
 
 # Input & Output folders
-input_folder = r"E:\unb-workstation\Writers All Novels\Deeba tabassum Novels\New folder"
+input_folder = r"E:\unb-workstation\Writers All Novels\Nazia Kamran Kashif Novels\pngs"
 output_folder = os.path.join(input_folder, 'thumbnails_1000x667')
 os.makedirs(output_folder, exist_ok=True)
 
